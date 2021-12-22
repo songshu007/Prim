d6c969edb35a1fd028ae2506932023ce4215b056
songshu007 <74942051+songshu007@users.noreply.github.com>
2021-12-22 20:45:54 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,6 +17,10 @@ const int BLOCK_HEIGHT = HEIGHT / (BLOCK_XY);//小方格y轴数量
 #define daixuan COLORREF RGB(0,255,0)//待选颜色
 #define xuanzhong COLORREF RGB(255,0,0)//选中颜色
 
+#define ROAD 0//路（空气）
+#define WALL 1//墙壁
+#define WAIT 2//待选路点
+
 struct block{
 public:
 	int x;
@@ -35,7 +39,7 @@ vector<block*> list;
 void clearscreen(COLORREF RGB);//清空地图为RGB颜色(带有网格)
 void gamedrawpixal(int x, int y, COLORREF RGB);//在x，y位置画颜色为RGB的方格
 void init();//初始化
-void join(int x, int y);//将x,y四周不是路的路点加入待选列表
+void join(int x, int y);//将x,y四周是墙壁的路点加入待选列表
 void display();//刷新屏幕
 void prim_main();//prim主循环
 void through(int x, int y);//将x,y与它四周一个随机已经变成路的路点打通
@@ -46,11 +50,11 @@ void init() {
 	/*地图初始化为墙*/
 	for (int y = 0; y < BLOCK_HEIGHT; y++) {
 		for (int x = 0; x < BLOCK_WIDTH; x++) {
-			map[x][y] = 1;
+			map[x][y] = WALL;
 		}
 	}
 	/*随机选一个路点，将它变成路*/
-	map[40][40] = 0;
+	map[40][40] = ROAD;
 	/*将路点四周不是路的路点加入待选列表*/
 	join(40, 40);
 	prim_main();
@@ -63,7 +67,7 @@ void prim_main() {
 		/*将A与它四周一个随机已经变成路的路点打通*/
 		through(A->x, A->y);
 		/*将A变成路*/
-		map[A->x][A->y] = 0;
+		map[A->x][A->y] = ROAD;
 		/*将A四周不是路的路点加入待选列表*///(注意，不要重复添加路点！)
 		join(A->x, A->y);
 		/*从待选路点中移除A*/
@@ -78,66 +82,66 @@ void prim_main() {
 void through(int x, int y) {
 	list.clear();
 	//上
-	if (y - 2 >= 0 && map[x][y - 2] == 0) {
+	if (y - 2 >= 0 && map[x][y - 2] == ROAD) {
 		block* a = new block(x, y - 1);
 		list.push_back(a);
 	}
 	//下
-	if (y + 2 <= BLOCK_HEIGHT - 1 && map[x][y + 2] == 0) {
+	if (y + 2 <= BLOCK_HEIGHT - 1 && map[x][y + 2] == ROAD) {
 		block* b = new block(x, y + 1);
 		list.push_back(b);
 	}
 	//左
-	if (x - 2 >= 0 && map[x - 2][y] == 0) {
+	if (x - 2 >= 0 && map[x - 2][y] == ROAD) {
 		block* c = new block(x - 1, y);
 		list.push_back(c);
 	}
 	//右
-	if (x + 2 <= BLOCK_WIDTH - 1 && map[x + 2][y] == 0) {
+	if (x + 2 <= BLOCK_WIDTH - 1 && map[x + 2][y] == ROAD) {
 		block* d = new block(x + 1, y);
 		list.push_back(d);
 	}
 	int BIndexea = rand() % list.size();
 	block* B = list[BIndexea];
 	/*将x，y与B打通*/
-	map[B->x][B->y] = 0;
+	map[B->x][B->y] = ROAD;
 }
 void join(int x, int y) {
 	//上
-	if (y - 2 >= 0 && map[x][y - 2] == 1) {
+	if (y - 2 >= 0 && map[x][y - 2] == WALL) {
 		block* a = new block(x, y - 2);
 		openlist.push_back(a);
-		map[x][y - 2] = 2;
+		map[x][y - 2] = WAIT;
 	}
 	//下
-	if (y + 2 <= BLOCK_HEIGHT - 1 && map[x][y + 2] == 1) {
+	if (y + 2 <= BLOCK_HEIGHT - 1 && map[x][y + 2] == WALL) {
 		block* b = new block(x, y + 2);
 		openlist.push_back(b);
-		map[x][y + 2] = 2;
+		map[x][y + 2] = WAIT;
 	}
 	//左
-	if (x - 2 >= 0 && map[x - 2][y] == 1) {
+	if (x - 2 >= 0 && map[x - 2][y] == WALL) {
 		block* c = new block(x - 2, y);
 		openlist.push_back(c);
-		map[x - 2][y] = 2;
+		map[x - 2][y] = WAIT;
 	}
 	//右
-	if (x + 2 <= BLOCK_WIDTH - 1 && map[x + 2][y] == 1) {
+	if (x + 2 <= BLOCK_WIDTH - 1 && map[x + 2][y] == WALL) {
 		block* d = new block(x + 2, y);
 		openlist.push_back(d);
-		map[x + 2][y] = 2;
+		map[x + 2][y] = WAIT;
 	}
 }
 void display() {
 	for (int y = 0; y < BLOCK_HEIGHT; y++) {
 		for (int x = 0; x < BLOCK_WIDTH; x++) {
-			if (map[x][y] == 1) {
+			if (map[x][y] == WALL) {
 				gamedrawpixal(x, y, qiangbi);
 			}
-			else if (map[x][y] == 0) {
+			else if (map[x][y] == ROAD) {
 				gamedrawpixal(x, y, kongqi);
 			}
-			else if (map[x][y] == 2) {
+			else if (map[x][y] == WAIT) {
 				gamedrawpixal(x, y, daixuan);
 			}
 		}
@@ -156,18 +160,15 @@ void gamedrawpixal(int x, int y, COLORREF RGB) {
 	fillrectangle(x * BLOCK_XY, y * BLOCK_XY, BLOCK_XY + x * BLOCK_XY, BLOCK_XY + y * BLOCK_XY);
 }
 int main() {
-	srand((unsigned)time(NULL));	//随机数种子
+	srand((unsigned)time(NULL));//随机数种子
 	initgraph(WIDTH, HEIGHT);//创建绘图窗口
 	BeginBatchDraw();//开始批量绘图
 	cleardevice();//清空显示
 
-
 	init();
+
 	display();
-	//gamedrawpixal(0, BLOCK_HEIGHT - 1, xuanzhong);
 	FlushBatchDraw();//将缓冲输出到屏幕
 	
-
-
-	while (1);
+	while(1);
 }
